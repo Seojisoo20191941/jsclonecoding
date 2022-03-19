13ceb02904326d0b64f38f1feeabec8706e5a12a
서지수 <seojisoo@seojisuui-MacBookPro.local>
2022-03-19 17:13:59 +0900

    reviewed js theory

---
+++ theory/app.js
@@ -0,0 +1,43 @@
+//변수
+const a=5; // 변하지 않는 값
+let b=2; // 변할 수 있는 값_업데이트 허용
+b=6;
+var c=1; // 변할 수 있는 값_업데이트 허용. But 옛날에 존재한 변수라서 웬만하면 쓰지 마. 업데이트 되어도 됐다는 걸 알려주지 않음 
+
+//array 배열 
+const daysOfWeek=["mon"];
+daysOfWeek.push("Tue"); //배열 추가
+
+//Object. 일일이 하나씩 선언하는 것보다 훨씬 편함
+const player={
+    name:"jisoo",
+    points:10,
+    fat:true,
+};
+console.log(player);
+console.log(player.name);
+player.points=15; //이런식으로 추가 가능!
+
+//function
+function sayHello(nameOfPerson, age) {
+    console.log(nameOfPerson);
+}
+sayHello("soo",24);
+
+const player={
+    name:"soo",
+    sayHello:function(otherName){
+        console.log("hi"+otherName)
+    },
+};
+player.sayHello("jisoo");
+player.name="soosoo"; //이름 변경
+
+//recap
+const sum={
+    a:function(numOne, numTwo){
+        return numOne+numTwo;
+    },
+};
+
+let plus=sum.a(1,2);
